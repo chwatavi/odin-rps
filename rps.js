344f215d8871bb6200f85da415c93f9da46ec157
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 01:27:16 +0100

    Write getComputerChoise function

--- rps.js
+++ rps.js
@@ -1,4 +1,13 @@
 function getComputerChoice(){
   // Return randomly either Rock, Paper or Scisssors.
-  switch (getRandomInt)
+  switch (getRandomInt(3)){
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    default:
+      return "Scissors";
+  }
 }
