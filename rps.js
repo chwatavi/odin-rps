b546a24323d99dda2e07887045f90a9f236ebdde
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 01:46:46 +0100

    Implement choices to numbers conversion

--- rps.js
+++ rps.js
@@ -1,13 +1,40 @@
-function getComputerChoice(){
-  // Return randomly either Rock, Paper or Scisssors.
-  switch (Math.floor(Math.random() * 3)){
+function choiceToNum(choice){
+  // Convert Rock, Paper, Scissors to 0, 1, 2 respectively
+  // Output is null if the input is invalud
+  switch (choice.toLowerCase()){
+    case "rock":
+      return 0;
+      break;
+    case "paper":
+      return 1;
+      break;
+    case "scissors":
+      return 2;
+      break;
+    default:
+      return null;
+  }
+}
+
+function numToChoice(num){
+  // Convert number 0, 1, 2 to Rock, Paper, Scissors respectively
+  switch (num){
     case 0:
       return "Rock";
       break;
     case 1:
       return "Paper";
       break;
-    default:
+    case 3:
       return "Scissors";
+      break;
+    default:
+      return null;
   }
+}
+
+function getComputerChoice(){
+  // Return randomly either Rock, Paper or Scisssors
+  // Use an expression that reandomly generates 0, 1 or 2
+  return numToChoice (Math.floor(Math.random() * 3));
 }
